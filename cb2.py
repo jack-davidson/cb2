5c30b928b49541c58efc86c6b22e5b630bdbf00f
rachitkakkar <71226802+rachitkakkar@users.noreply.github.com>
2021-04-22 08:42:08 -0500

    Update cb2.py

--- cb2.py
+++ cb2.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
 import requests
 import tokens
 
 url = "https://machinelearningforkids.co.uk/api/scratch/" + tokens.key + "/classify"
-
 
 # This function will pass your text to the machine learning model
 # and return the top result with the highest confidence
@@ -20,17 +18,18 @@ def classify(text):
 
 
 def respond(label, confidence):
-    categories_responses = [
-        {
-            "Category": "EndOfConversation",
-            "Response": "We are glad that you got the help you needed, bye!"
-        }
-    ]
+    categories_responses = [{"Category": "InternetError", "Response": "Please Use This Official Verizon TroubleShooter To Fix Your Internet Issue (https://www.verizon.com/foryourhome/vzrepair/flowengine/UFDService.aspx?Keyword=FIX_CCON)."}, 
+                            {"Category": "EndOfConversation", "Response": "We are glad that you got the help you needed, bye!"},
+                            {"Category": "CustomerNeedsHelp", "Response": "We hear your concerns about not being able to reach customer service, and are forwarding you to an expert."},
+                            {"Category": "BillingError", "Response": "Please Use This Guide To Pay Your Bill (https://www.verizon.com/support/residential/account/pay-bill/how-to)."},
+                            {"Category": "InternetSpeedError", "Response": "Please Use This Speed Test (https://www.verizon.com/speedtest/) while I forward you to an expert!"},
+                            {"Category": "CableError", "Response": "Please Use This Official Verizon TroubleShooter To Fix Your Cable/TV issue (https://www.verizon.com/business/support/fios-tv/troubleshooting)."}]
+
     for category_response in categories_responses:
         category = category_response["Category"]
         response = category_response["Response"]
 
-        if label == category and confidence > 35:
+        if label == category and confidence > 10:
             return response
 
     return "I'm sorry, I can't understand. You will be forwarded to a human expert soon."
@@ -42,5 +41,5 @@ while True:
 
     label = model_input["class_name"]
     confidence = model_input["confidence"]
-
+    
     print(respond(label, confidence))
